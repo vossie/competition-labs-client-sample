22040500fc92a3079bcff2c0a72724362ed366ee
vossie <carel.vosloo@bronzegate.com>
2016-01-08 09:59:19 +0000

    Updated the remote service port

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,7 @@ public class Main {
     public static final String  API_KEY     = "REPLACE-ME-WITH-YOUR-API-KEY";
 
     public static final String  ROOT_URI    = "https://app.competitionlabs.com";
-    public static final Integer PORT        = 9443;
+    public static final Integer PORT        = 443;
 
     public static final Connection HTTP_CONNECTION = new Connection(ROOT_URI, SPACE_NAME, API_KEY, PORT);
 
